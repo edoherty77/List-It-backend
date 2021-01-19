fac215a628c4aa730235c0c1d68e2e67cdd5f5e7
Evan Doherty <edoherty77@gmail.com>
2021-01-19 14:30:38 -0500

    models added

---
+++ models/item.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const ItemSchema = new Schema({
+  title: String,
+  imageUrl: String,
+  email: String,
+  description: String,
+  price: String,
+  category: String,
+  dateAdded: String,
+})
+
+const Item = mongoose.model('Item', ItemSchema)
+
+module.exports = Item
